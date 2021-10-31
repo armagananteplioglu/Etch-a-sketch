61f2d3b9fc04444f8126ee0a60df05af2c7d60d0
Armagan Anteplioglu <armagananteplioglu@gmail.com>
2021-10-31 06:33:03 +0200

    day2

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function sliderValueReader(val) {
-    document.getElementById('sliderInput').value=val; 
-  }
+let n = prompt ("Please specify grid size");
 
+for (i = 0; i < n*n; i++){
+    const div = document.createElement ("div");
+    div.classList.add ("gridItem");
+    gridContainer = document.getElementById("container");
+    gridContainer.appendChild(div)
+//    gridContainer.setProperty ("grid-template-columns", "repeat(n, 100px)")
+//    gridContainer.setProperty ("grid-template-rows", "repeat(n, 100px)")
+}
