724f561dba8a290e39e7da70d8b3a077de16a2c3
Armagan Anteplioglu <armagananteplioglu@gmail.com>
2021-11-03 05:15:02 +0200

    day4

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
 let n = prompt ("Please specify grid size");
 gridContainer = document.getElementById("container");
 const item = document.getElementsByClassName ("gridItem")
+const resetButton = document.getElementById ("reset-btn")
 
 //grid maker
-
-for (i = 0; i < n*n; i++){
-    const div = document.createElement ("div");
-    div.classList.add ("gridItem");
-    gridContainer.appendChild(div)
-}   
-
-for (c = 0; c < n; c++) {
-    gridContainer.style.gridTemplateColumns = (`repeat(${n}, 1fr`)
-    gridContainer.style.gridTemplateRows = (`repeat(${n}, 1fr`)
+function gridMaker (n){
+    for (i = 0; i < n*n; i++){
+        const div = document.createElement ("div");
+        div.classList.add ("gridItem");
+        gridContainer.appendChild(div)
+    }   
+    
+    for (c = 0; c < n; c++) {
+        gridContainer.style.gridTemplateColumns = (`repeat(${n}, 1fr)`)
+        gridContainer.style.gridTemplateRows = (`repeat(${n}, 1fr)`)
+    }
 }
 
+gridMaker(n);
 
 //painter
 
-item.addEventListener ("hover", paint());
+for (let i = 0 ; i < item.length; i++) {
+    item[i].addEventListener('mouseover' , painter , false ) ; 
+}
 
-function paint() {
-    item.style.backgroundColor = "black";
-    return false;
+function painter (item) {
+    item.srcElement.style.backgroundColor = "black";
+}
+
+// cleaner
+resetButton.addEventListener ("click", clean)
+
+function clean () {
+    for (let i = 0 ; i < item.length; i++) {
+        item[i].style.backgroundColor = "white"
+    }
 }
